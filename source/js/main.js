5fa3289c7906f0683d7840c473ac1fcd097b692d
matsuda <matsuda.ddcm@gmail.com>
2016-01-07 18:41:23 +0900

    テキストの入力対応

--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,17 @@
 var app = angular.module('SvgApp',[]);
-    app.controller('SvgController', function () {
+app.controller('SvgController', function () {
 
     var self = this;
     var svgWidth = 514;
-    var svgHeight = 726;
+    var svgHeight = 514;
     var centerX = svgWidth / 2;
     var centerY = svgHeight / 2;
+    var svgId = 'svgBox';
+    var svgContainer = document.getElementById('svgContainer');
+    var resultSpace = document.getElementById('resultArea');
+    var resultCode;
 
-
-
-    var svgBox = Snap('#svgBox').attr({
+    var svgBox = Snap('#' + svgId).attr({
         width: svgWidth,
         height: svgHeight,
         viewBox: '0,0,' + svgWidth + ',' + svgHeight
@@ -44,33 +46,166 @@ var app = angular.module('SvgApp',[]);
             })
             .drag();
 
+    };
 
+
+
+
+    self.circleAttrReplace = function (num) {
+        var timer = setTimeout(function(){
+            var targetCircle = Snap('#circle' + num);
+            targetCircle.attr({
+                r: self.circleList[num].circleR,
+                fill: self.circleList[num].circleFill,
+                stroke: self.circleList[num].circleBC,
+                strokeWidth: self.circleList[num].circleBW,
+                opacity: self.circleList[num].circleOpacity
+            });
+        }, 200);
+    };
+
+
+
+    self.rectList = [];
+
+    self.createRect = function () {
+        self.rectList.push({
+            rectW: 40,
+            rectH: 40,
+            rectX: centerX,
+            rectY: centerY,
+            rectFill: "#000000",
+            rectBW: 4,
+            rectBC: "#000000",
+            rectOpacity: 1,
+            rectDegr: 0
+        });
+
+        self.rects = svgBox.rect()
+            .attr({
+                width: self.rectList[self.rectList.length - 1].rectW,
+                height: self.rectList[self.rectList.length - 1].rectH,
+                x: self.rectList[self.rectList.length - 1].rectX,
+                y: self.rectList[self.rectList.length - 1].rectY,
+                fill: self.rectList[self.rectList.length - 1].rectFill,
+                stroke: self.rectList[self.rectList.length - 1].rectBC,
+                strokeWidth: self.rectList[self.rectList.length - 1].rectBW,
+                opacity: self.rectList[self.rectList.length - 1].rectOpacity,
+                id: "rect" + (self.rectList.length - 1)
+            })
+            .drag();
+
+
+    };
+
+
+    self.rectAttrReplace = function (num) {
+        var timer = setTimeout(function(){
+            var targetRect = Snap('#rect' + num);
+            targetRect.attr({
+                width: self.rectList[num].rectW,
+                height: self.rectList[num].rectH,
+                fill: self.rectList[num].rectFill,
+                stroke: self.rectList[num].rectBC,
+                strokeWidth: self.rectList[num].rectBW,
+                opacity: self.rectList[num].rectOpacity
+            });
+        }, 200);
+    };
+
+
+
+
+    self.starList = [];
+
+    self.createStar = function () {
+        self.starList.push({
+            fontSize: 60,
+            starC: '#000000',
+            starOpacity: 1
+            //starDegr: 0
+        });
+
+        self.stars = svgBox.text(centerX, centerY, '★')
+            .attr({
+                fontSize: self.starList[self.starList.length - 1].fontSize + 'px',
+                fill: self.starList[self.starList.length - 1].starC,
+                opacity: self.starList[self.starList.length - 1].starOpacity,
+                textAnchor: 'middle',
+                id: 'star' + (self.starList.length - 1)
+            })
+            .drag();
 
 
     };
 
 
 
+    self.starAttrReplace = function (num) {
 
-        self.circleAttrReplace = function (num) {
-            var timer = setTimeout(function(){
-                var targetCircle = Snap('#circle' + num);
-                targetCircle.attr({
-                    r: self.circleList[num].circleR,
-                    fill: self.circleList[num].circleFill,
-                    stroke: self.circleList[num].circleBC,
-                    strokeWidth: self.circleList[num].circleBW,
-                    opacity: self.circleList[num].circleOpacity
-                }, 200);
+        var timer = setTimeout( function () {
+            var targetStar = Snap('#star' + num);
+            targetStar.attr({
+                fontSize: self.starList[num].fontSize + 'px',
+                fill: self.starList[num].starC,
+                opacity: self.starList[num].starOpacity
             });
-        };
+
+        }, 200);
+    };
 
 
 
 
+    self.textList = [];
 
+
+    self.insertText = function () {
+
+        self.textList.push({
+            valText: 'テキスト',
+            fontC: '#000000',
+            fontSize: 60,
+            textOpacity: 1
+        });
+
+        self.texts = svgBox.text(centerX, centerY, self.textList[self.textList.length - 1].valText)
+            .attr({
+                fill: self.textList[self.textList.length - 1].fontC,
+                fontSize: self.textList[self.textList.length - 1].fontSize,
+                opacity: self.textList[self.textList.length - 1].textOpacity,
+                textAnchor: 'middle',
+                id: 'text' + (self.textList.length - 1)
+            })
+            .drag();
+
+    };
+
+    self.textAttrReplace = function (num) {
+
+        var timer = setTimeout( function () {
+
+
+            var setText = document.getElementById('text' + num);
+
+            setText.innerHTML = self.textList[num].valText;
+
+
+            var targetText = Snap('#text' + num);
+
+            targetText.attr({
+                fontSize: self.textList[num].fontSize + 'px',
+                fill: self.textList[num].fontC,
+                opacity: self.textList[num].textOpacity
+            });
+
+
+        }, 200);
+    };
+
+    self.distResult = function () {
+        resultCode = String(svgContainer.innerHTML);
+        resultSpace.innerText = resultCode;
+    }
 
 });
-
-
-
